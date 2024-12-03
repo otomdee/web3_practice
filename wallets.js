9c149e48be008f0d55bb75c7e368fb069b595a3a
otomdee <otomdaniel65@gmail.com>
2024-12-03 01:45:15 +0100

    signers and sending txs

--- wallets.js
+++ wallets.js
@@ -1,10 +1,24 @@
-import { ethers } from "ethers";
+import { ethers, formatEther } from "ethers";
+import 'dotenv/config'
 
-const myWallet = ethers.Wallet.createRandom();
-const seed = myWallet.mnemonic.phrase;
+const infuraId = process.env.INFURA_KEY;
+const infuraUrl = `https://sepolia.infura.io/v3/${infuraId}`;
 
-const wallet7 = ethers.Wallet.fromPhrase(seed, {path: `m/44'/60'/0'/0/7`});
-const wallet4 = ethers.Wallet.fromPhrase(seed, {path: `m/44'/60'/0'/0/4`});
+const provider = new ethers.JsonRpcProvider(infuraUrl);
 
-console.log(wallet7.address);
-console.log(wallet4.address);
+const wallet = ethers.Wallet.fromPhrase(process.env.PRIVATE_KEY, provider);
+
+const myBalance = await provider.getBalance(wallet.address);
+
+const tx = await wallet.sendTransaction(
+    {
+        to: "0x4f3C4d972600D23C8dF09E736d3C4D32AB219Ec9",
+        value: myBalance / 5n,
+    }
+)
+
+console.log("sending tx...", tx);
+
+await tx.wait();
+
+console.log("tx sent");
